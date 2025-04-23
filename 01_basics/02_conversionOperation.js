898b8a9dd991f505642f354ca64adee99fc07676
Manaswini Padhi <112185337+Manaswini-0404@users.noreply.github.com>
2025-04-23 06:18:29 +0000

    Operation conversion

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,115 @@
+//let score="33"
+
+//console.log(typeof score); 
+//console.log(typeof(score));  
+
+/*let valueInNumber=Number(score)   
+console.log(typeof valueInNumber);*/ 
+
+/*let score="33abc"
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); */
+
+/*let score=null
+
+console.log(typeof score); 
+console.log(typeof(score)); 
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); 
+*/
+/*
+let score=true
+
+console.log(typeof score); 
+console.log(typeof(score)); 
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber);    */
+
+
+/*let score="Manu"
+
+console.log(typeof score); 
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber); 
+console.log(valueInNumber); 
+*/
+
+//"33"=>33
+//"33abc"=>NaN
+// true=>1;false=>0
+
+/*let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);  
+*/
+
+/*let isLoggedIn=""
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);  
+*/
+
+/*et isLoggedIn="Manu"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); 
+*/
+/*let Number=33
+
+let stringNum=String(Number)
+console.log(stringNum); 
+console.log(typeof stringNum);  
+*/
+
+/*let value=3
+let negValue=-value
+console.log(negValue);
+
+let str1="hello"
+let str2=" Manu"
+
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2);  
+console.log(1+"2");  
+console.log("1"+"2"); 
+console.log("1"+2+2);  
+console.log(1+2+"2");   
+*/
+/*console.log(true); 
+console.log(true+); 
+console.log(+true); 
+*/
+
+//console.log(+""); 
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
