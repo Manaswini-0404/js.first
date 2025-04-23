78037a2ab60a3efd1e2d7dd35b6858c61a353934
Manaswini Padhi <112185337+Manaswini-0404@users.noreply.github.com>
2025-04-23 06:14:06 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail="manaswini.padhi44@gmail.com"
+var accountPassword="123456"
+accountCity="Rayagada"
+
+accountEmail="manu@gmail.com"
+accountPassword="1233"
+accountCity="Berhampur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity]);
