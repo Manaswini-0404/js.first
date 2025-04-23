95fa217c2b6c49053391bc00e70fff051b4abb41
Manaswini Padhi <112185337+Manaswini-0404@users.noreply.github.com>
2025-04-23 15:12:22 +0000

    Array-1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,34 @@
+const myArr=[1,2,3,5,9,6]
+//console.log(myArr[3]);
+
+//const myHeros=["shaktiman","vikrambetaal"]
+
+//const myArr2=new Array(1,2,3,4)
+
+//myArr.push(6) //add element to array
+//console.log(myArr);
+//myArr.pop() //remove the last element
+//console.log(myArr);
+
+//myArr.unshift(9) 
+//console.log(myArr);
+
+//myArr.shift() 
+//console.log(myArr); 
+
+//const newArr=myArr.join()
+//console.log(myArr);
+//console.log(newArr);
+
+console.log("A ",myArr);
+
+const myArr1=myArr.slice(1,3)
+console.log(myArr1);
+console.log("B ",myArr);
+
+const myArr2=myArr.splice(1,3)
+console.log(myArr2);
+console.log("C ",myArr);
+
+
+
